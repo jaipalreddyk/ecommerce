60651b7ce5019274290a2e9cb4d7d51953a3dc2a
krishna7362 <52160994+krishna7362@users.noreply.github.com>
2019-07-25 15:57:40 +0530

    Update runapp.bat

--- runapp.bat
+++ runapp.bat
@@ -1,1 +1,2 @@
-xcopy "C:\Program Files (x86)\Jenkins\workspace\ecommerce_master\target\ecommerce.war"  "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps" -force
+del C:\Program Files (x86)\Jenkins\workspace\ecommerce_master\target\ecommerce.war
+xcopy "C:\Program Files (x86)\Jenkins\workspace\ecommerce_master\target\ecommerce.war" "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps" -force
